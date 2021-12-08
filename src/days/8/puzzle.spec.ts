67a6047c19529a2f7b231540d4859aeb7563d900
Francesco Maida <francescomaida91@gmail.com>
2021-12-08 18:00:42 +0100

    feat: added day 8

---
+++ src/days/8/puzzle.spec.ts
@@ -0,0 +1,13 @@
+import PuzzleFactory from '../../utils/PuzzleFactory';
+
+describe('day 8', () => {
+  test('puzzle 1', async () => {
+    const puzzle = await PuzzleFactory.getPuzzle('8', 'test');
+    expect(puzzle.solveFirst()).toEqual(puzzle.getFirstExpectedResult());
+  });
+
+  test('puzzle 2', async () => {
+    const puzzle = await PuzzleFactory.getPuzzle('8', 'test');
+    expect(puzzle.solveSecond()).toEqual(puzzle.getSecondExpectedResult());
+  });
+});
